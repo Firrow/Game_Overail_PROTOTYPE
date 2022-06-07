e45f8173bc32a99bc98b10eea7ea7844909b7382
Firrow <73218766+Firrow@users.noreply.github.com>
2022-06-07 15:58:28 +0200

    New tests map + loop parameter
    Add of loop parameter in PlayerMovement
    Creation of a new map to do tests

--- unity_over_rail/Assets/Scripts/Player/playerMovement.cs
+++ unity_over_rail/Assets/Scripts/Player/playerMovement.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class playerMovement : MonoBehaviour
 {
     [SerializeField]
     private Transform[] _road;
 
-    private int _roadToGo;
+    private int _indexRoadToGo;
     private float _tParam;
     private Vector3 _objectPosition;
-    public float speed;
     private bool _coroutineAllowed;
+
+    public float speed;
+    public bool loop;
 
     // Start is called before the first frame update
     void Start()
     {
-        _roadToGo = 0;
+        _indexRoadToGo = 0;
         _tParam = 0f;
         _coroutineAllowed = true;
     }
@@ -26,7 +29,7 @@ public class playerMovement : MonoBehaviour
     {
         if (_coroutineAllowed)
         {
-            StartCoroutine(GoByTheRoute(_roadToGo));
+            StartCoroutine(GoByTheRoute(_indexRoadToGo));
         }
     }
 
@@ -34,15 +37,21 @@ public class playerMovement : MonoBehaviour
     {
         _coroutineAllowed = false;
 
-        //récupération des positions des points
+        //récupération des positions des points dans bon sens
         Vector3 p0 = _road[roadNum].GetChild(0).position;
         Vector3 p1 = _road[roadNum].GetChild(1).position;
         Vector3 p2 = _road[roadNum].GetChild(2).position;
         Vector3 p3 = _road[roadNum].GetChild(3).position;
 
+        //récupération des positions des points dans sens inverse
+        /*Vector3 p3 = _road[roadNum].GetChild(0).position;
+        Vector3 p2 = _road[roadNum].GetChild(1).position;
+        Vector3 p1 = _road[roadNum].GetChild(2).position;
+        Vector3 p0 = _road[roadNum].GetChild(3).position;*/
+
         while (_tParam < 1)
         {
-            _tParam += Time.deltaTime * speed;
+            _tParam += Time.deltaTime * speed ;
 
             //la position de la forme prend la valeur de la courbe
             _objectPosition = Mathf.Pow(1 - _tParam, 3) * p0 +
@@ -58,12 +67,12 @@ public class playerMovement : MonoBehaviour
         //MAJ des paramètres après le déplacement
         _tParam = 0;
         speed = speed * 0.90f;
-        _roadToGo += 1;
+        _indexRoadToGo += 1;
 
         //loop du chemin
-        if (_roadToGo > _road.Length - 1)
+        if (loop && (_indexRoadToGo > _road.Length - 1))
         {
-            _roadToGo = 0;
+            _indexRoadToGo = 0;
         }
 
         _coroutineAllowed = true;
