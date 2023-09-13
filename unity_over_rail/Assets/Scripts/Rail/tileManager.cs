e154bad388b7446bfe2ab4bdc104282293bffc5f
Firrow <firrowmb@gmail.com>
2023-09-13 23:09:07 +0200

    test trigger first road deactivate

--- unity_over_rail/Assets/Scripts/Rail/tileManager.cs
+++ unity_over_rail/Assets/Scripts/Rail/tileManager.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class tileManager : MonoBehaviour
 {
     public string directionOfTile;
-    
+    private GameObject[] tiles;
+    public Transform TileTrigger;
+
+    void Start()
+    {
+        tiles = GameObject.FindGameObjectsWithTag("FirstRoad");
+        TileTrigger = this.gameObject.transform.GetChild(0);
+    }
+
     public string DirectionOfTile(GameObject tile)
     {
         return tile.GetComponent(directionOfTile).ToString();
@@ -13,4 +24,8 @@ public class tileManager : MonoBehaviour
 
     //Désactiver route avec tag "FirstRoad" lorsque les joueurs sont définitivement rentrés sur le terrain
     //mettre animation barrière qui se baisse
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+    }
 }
