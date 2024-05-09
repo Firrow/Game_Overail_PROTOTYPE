9dc99c9e47cad99232b1112d47ec20993a1781d4
Firrow <firrowmb@gmail.com>
2024-05-09 12:11:23 +0200

    Adding quit button for tests

--- unity_over_rail/Assets/Scripts/Player/HumanTrain/PlayerInputHandler.cs
+++ unity_over_rail/Assets/Scripts/Player/HumanTrain/PlayerInputHandler.cs
@@ -32,4 +32,11 @@ public class PlayerInputHandler : MonoBehaviour
         if (humanTrain != null)
             humanTrain.playerShoot(context);
     }
+
+    //TEMPORAIRE POUR TESTS
+    public void OnQuit(InputAction.CallbackContext context)
+    {
+        if (humanTrain != null)
+            Application.Quit();
+    }
 }
